639b7177507fc4e09739a6f8aa6b55ee85b967a5
melodytowett <melodytowett99@gmail.com>
2022-10-11 22:04:41 +0300

    first springboot project

---
+++ src/main/java/com/example/tuesdaytrial/TuesdaytrialApplication.java
@@ -0,0 +1,13 @@
+package com.example.tuesdaytrial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuesdaytrialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TuesdaytrialApplication.class, args);
+    }
+
+}
